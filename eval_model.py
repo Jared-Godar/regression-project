b83e5547837f5795aceb01c5974e0708719fb339
Jared Godar <jared.godar@gmail.com>
2021-12-13 11:26:03 -0600

    Commit final changes

--- eval_model.py
+++ eval_model.py
@@ -28,6 +28,7 @@ warnings.filterwarnings("ignore")
 def correlation_exploration(df, x_string, y_string):
     r, p = stats.pearsonr(df[x_string], df[y_string])
     ax= sns.regplot(x=x_string, y=y_string, data=df, line_kws={"color": "red"})
+    plt.figure(figsize=(16,8))
     plt.title(f"{x_string}'s Relationship with {y_string}")
     print(f'The p-value is: {p:4f}. There is {round(p,3)}% chance that we see these results by chance.')
     print(f'r = {round(r, 2)}')
